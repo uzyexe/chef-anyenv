3ff64325fe5620fe7caba23211f1148258ff0ded
uzyexe <uzy.exe@gmail.com>
2016-11-27 20:54:18 +0900

    bump up 0.0.3

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'uzy.exe@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures anyenv'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.2'
+version          '0.0.3'
